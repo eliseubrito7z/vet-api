ed19918f8980e2ca818a8a2c509eea9a3d821ae5
Eliseu Brito <eliseubrito776@gmail.com>
2023-03-17 23:34:15 -0300

    feat: StaffController with PostMapping for create staff and service with insert method

--- vet-group/src/main/java/com/vet/vetgroup/controllers/StaffController.java
+++ vet-group/src/main/java/com/vet/vetgroup/controllers/StaffController.java
@@ -1,16 +1,18 @@
 package com.vet.vetgroup.controllers;
 
+import com.vet.vetgroup.dtos.creation.StaffCreationDto;
 import com.vet.vetgroup.models.Staff;
 import com.vet.vetgroup.services.StaffService;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -20,6 +22,21 @@ public class StaffController {
 
     @Autowired
     private StaffService service;
+
+    @PostMapping(value = "/create")
+    public ResponseEntity<Long> create(@RequestBody @Valid StaffCreationDto dto) {
+        return ResponseEntity.ok().body(service.insert(dto));
+    }
+
+    @PatchMapping(value = "/onDuty")
+    public ResponseEntity<Void> updateOnDuty(
+            @RequestParam Boolean onDuty,
+            @RequestHeader(HttpHeaders.AUTHORIZATION) String token
+    ) {
+//        Staff staff = service.findById();
+        return ResponseEntity.ok().build();
+    }
+
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<Staff>> findAll() {
@@ -31,5 +48,11 @@ public class StaffController {
     public ResponseEntity<Staff> findById(@PathVariable Long id) {
         Staff staff = service.findById(id);
         return ResponseEntity.status(HttpStatus.OK).body(staff);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable Long id) {
+        service.deleteById(id);
+        return ResponseEntity.ok().build();
     }
 }
--- vet-group/src/main/java/com/vet/vetgroup/services/StaffService.java
+++ vet-group/src/main/java/com/vet/vetgroup/services/StaffService.java
@@ -1,10 +1,13 @@
 package com.vet.vetgroup.services;
 
+import com.vet.vetgroup.dtos.creation.StaffCreationDto;
 import com.vet.vetgroup.models.Staff;
 import com.vet.vetgroup.repositories.StaffRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -24,6 +27,14 @@ public class StaffService {
         if(staff.isEmpty()) throw new IllegalArgumentException("This Staff not exists");
 
         return staff.get();
+    }
+
+    public Long insert(StaffCreationDto dto) {
+        Staff staffModel = new Staff();
+        BeanUtils.copyProperties(dto, staffModel);
+        staffModel.setCreatedAt(new Date());
+        repository.save(staffModel);
+        return staffModel.getId();
     }
 
     public void deleteById(Long id) {
