90d0744174d71e741d6ee92a57a270daf5b0357a
Eliseu Brito <eliseubrito776@gmail.com>
2023-03-26 12:27:40 -0300

    refactor: change id generationType of AUTO for IDENTITY

--- vet-group/src/main/java/com/vet/vetgroup/models/RoleHistoric.java
+++ vet-group/src/main/java/com/vet/vetgroup/models/RoleHistoric.java
@@ -13,7 +13,7 @@ public class RoleHistoric  implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(unique = false, nullable = false, updatable = false)
     private Date startedIn;
